d81fc8bb34a84a19f10eefe8fb073a1229ce7f4b
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-08-23 17:16:41 -0300

    Reenviando o requisito 4, pois não havia chamado ele :)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   state = {
@@ -51,6 +52,16 @@ class App extends React.Component {
           // onSaveButtonClick={ onSaveButtonClick }
           onInputChange={ this.onInputChange }
           hasTrunfo={ hasTrunfo }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
         />
       </div>
     );
