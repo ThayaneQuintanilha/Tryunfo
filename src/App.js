772281fc5a0e2ae6a837a557c5af5a94b9e90841
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-08-24 12:34:24 -0300

    Requisito 6 feito

--- src/App.js
+++ src/App.js
@@ -41,6 +41,19 @@ class App extends React.Component {
     });
   };
 
+  clickButton = () => {
+    this.setState({ cardName: '',
+      cardDescription: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: true });
+  };
+
   render() {
     const {
       cardName,
@@ -68,6 +81,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           isSaveButtonDisabled={ isSaveButtonDisabled }
+          onSaveButtonClick={ this.clickButton }
           onInputChange={ this.onInputChange }
           hasTrunfo={ hasTrunfo }
         />
